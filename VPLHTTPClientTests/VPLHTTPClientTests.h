787601610587cd2f57de16e4d1a5b6d784ec43bb
Christian Niles <christian@nerdyc.com>
2011-05-17 14:05:52 -0700

    Initial Commit

---
+++ VPLHTTPClientTests/VPLHTTPClientTests.h
@@ -0,0 +1,17 @@
+//
+//  VPLHTTPClientTests.h
+//  VPLHTTPClientTests
+//
+//  Created by Christian Niles on 5/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface VPLHTTPClientTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ VPLHTTPClientTests/VPLHTTPClientTests.m
@@ -0,0 +1,33 @@
+//
+//  VPLHTTPClientTests.m
+//  VPLHTTPClientTests
+//
+//  Created by Christian Niles on 5/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "VPLHTTPClientTests.h"
+
+
+@implementation VPLHTTPClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in VPLHTTPClientTests");
+}
+
+@end
